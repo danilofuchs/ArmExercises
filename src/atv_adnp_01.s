4a30f971ceed1e4dc46ad8147017b3855e922bf0
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-08-24 16:34:51 -0300

    Initial step of bubble sort

--- src/atv_adnp_01.s
+++ src/atv_adnp_01.s
@@ -67,8 +67,8 @@ PrimeNumbersList
 
     bl checkIfBelowMaxAndPrime
 
-    ; ldr R4, =UNORDERED_PRIME_NUMBERS
-    ; bl bubbleSort
+    ldr R4, =UNORDERED_PRIME_NUMBERS
+    bl bubbleSort
 
     b endProgram
     
@@ -115,19 +115,33 @@ restartCheck
     mov R3, #2  ; R3 = 2
     b checkIfBelowMaxAndPrime
 
-; bubbleSort
-;     ldrb R6, [R4], #1
-;     ldrb R7, [R4]
-; 
-;     cmp R6, R7
-;     itte      hi
-;         movhi R8, R6       ; then
-;         movhi R6, R7       ; then
-;         movhi R7, R8       ; then
+bubbleSort
+    ldrb R6, [R4], #1 ; Load R4 into memory
+    ldrb R7, [R4]     ; Load next value
 
+    cmp R7, #0 ; Go back if end of array
+    bxeq LR
+
+    cmp R6, R7
+    push {LR} ; We need to save LR to go back at the end
+    blhi invertPositions ; If R6 > R7, should invert
+    pop {LR}
+
+    b bubbleSort
+
+invertPositions
+    mov R8, R6 ; Save to temporary reg
+    mov R6, R7
+    mov R7, R8
+
+    strb R6, [R4, #-1] ; Save new R6 into *(R4 - 1)
+    strb R7, [R4]      ; Save new R7 into *(R4)
+
+    bx LR
 
 NUMBERS_ARRAY DCB 0xC1,0x40,0xB0,0x7F,0x2B,0x13,0xD3,0x03,0xCB,0x05,0x15,0x7F,0xCE,0xF5,0x9D,0xED,0xF1,0x69,0xFC,0x13,0
 ;                  193;	 63; 176; 127;  43;  19; 211;   3; 203;   5;  21; 127; 206; 245; 157; 237; 241;	105; 252;  19, \0
-
+;         Ordered: 3, 5, 19, 19, 21, 43, 64, 105, 127, 127, 157, 176, 193, 203, 206, 211, 237, 241, 245, 252
+;     Ordered hex: ["3", "5", "13", "13", "15", "2b", "40", "69", "7f", "7f", "9d", "b0", "c1", "cb", "ce", "d3", "ed", "f1", "f5", "fc"]
     align
     end
